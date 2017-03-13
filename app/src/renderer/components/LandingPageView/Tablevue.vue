0975bfa95e0fc8cccdd110c688d363947fd1ee67
Henpai Hsu <dreamfliper@gmail.com>
2017-03-13 23:58:00 +0800

    fix previous commit

--- app/src/renderer/components/LandingPageView/Tablevue.vue
+++ app/src/renderer/components/LandingPageView/Tablevue.vue
@@ -1,14 +1,13 @@
 <template>
-  <data-table id="maintable" :data="getFiles" :striped=true :height="viewheight">
-    <column label="#" field="index" :width='5'></column>
-    <column label="姓名">
+  <data-table show-index id="maintable" :data="getFiles" :striped=true :height="viewheight">
+    <column label="姓名" :width='200'>
       <template scope="row">
         <span @click="change_selected(row.index)">{{ row.name }}</span>
       </template>
     </column>
-    <column label="address" :width='450'>
+    <column label="address">
       <template scope="row">
-        <span id="add" type="primary" @click="change_selected(row.index)">{{ row.add }}</span>
+        <span type="primary" @click="change_selected(row.index)">{{ row.add }}</span>
       </template>
     </column>
   <tag type="primary">{{getpflag}}</tag>
@@ -18,7 +17,6 @@
 
 <script>
 import {mapGetters, mapState, mapMutations} from 'vuex'
-
 // let   files= [
 //   { name: 'Learn JavaScript' ,add: 'sample1',content:'sample1'},
 //   { name: 'Learn Vue' ,add:'sample2',content:'sample2'},
@@ -49,15 +47,18 @@ export default {
   data (){
     return {
         // dataSource2:files
-        viewheight:window.innerHeight*0.45
+        viewheight:window.innerHeight-296
       }
    }
   }
-// console.log('tablevue debug message:'+window.innerHeight)
+console.log('tablevue debug message:'+window)
 </script>
 
 <style scoped>
 #maintable{
   margin: 0 5%;
+}
+span{
+  width: inherit;
 }
 </style>
